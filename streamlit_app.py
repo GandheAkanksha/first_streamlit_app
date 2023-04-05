80cb4d99c4e84d4c0f5cecb7a180a0961202f67c
GandheAkanksha <129280893+GandheAkanksha@users.noreply.github.com>
2023-04-06 00:57:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
 #create the repetable code block (called a function)
 def get_fruitvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
